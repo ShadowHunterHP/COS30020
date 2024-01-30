1bf07afeaf4532fe420de9c1769abdb0e2df3d96
ShadowHunterHP <nguyenanhvu.hp2003@gmail.com>
2024-01-30 23:11:14 +0700

    task 1

--- Lab 04/strprocess.php
+++ Lab 04/strprocess.php
@@ -4,28 +4,28 @@
 <meta charset="utf-8" />
 <meta name="description" content="Web application development" />
 <meta name="keywords" content="PHP" />
-<meta name="author" content="Your Name" />
+<meta name="author" content="Nguyen Anh Vu" />
 <title>TITLE</title>
 </head>
 <body>
     <h1>Web Programming - Lab 4</h1>
     <?php
-        if (isset ($_POST["strform.php"])) {
-            $str = $_POST["strform.php"];
+        if (isset ($_POST["data"])) {
+            $str = $_POST["data"];
             $pattern = "/^[A-Za-z ]+$/";
-        if (_______(3)________) {
-            $ans = "";
-            $len = _____(4)________;
-        for ($i = 0; $i < $len; $i++) {
-            $letter = substr ($str, (5), 1);
-            if ((strpos ("AEIOUaeiou", (6))) === false){
-                $ans = $ans . $letter;
+            if (preg_match($pattern, $str)) {
+                $ans = "";
+                $len = strlen($str);
+            for ($i = 0; $i < $len; $i++) {
+                $letter = substr ($str, $i, 1);
+                if ((strpos ("AEIOUaeiou", $i)) === false){
+                    $ans = $ans . $letter;
+                }
             }
-        }
-            echo "<p>The word with no vowels is ", $ans, ".</p>";
-        } else {
-            echo "<p>Please enter a string containing only letters or space.</p>";
-        }
+                echo "<p>The word with no vowels is ", $ans, ".</p>";
+            } else {
+                echo "<p>Please enter a string containing only letters or space.</p>";
+            }
         } else {
             echo "<p>Please enter string from the input form.</p>";
         }
